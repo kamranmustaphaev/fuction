03632d4f8e9dc4763801cde3e578af513c46dac1
Kamran <kamranmelitopol@gmail.com>
2022-11-20 19:39:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Задача 1.
+
+function nums (a, b) {
+ return a > b
+}
+console.log(nums(45, 44));
+
+// Задача 2.
+
+function nums() {
+    let arr = []
+    for(let i = 0;i <= 10; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
